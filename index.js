dc2cdbd78f9b3f7444217af80c8ac34454f6e55d
yacine-21 <yacinelyoubi37@gmail.com>
2022-06-27 12:51:27 +0200

    add .env file

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const express = require("express");
 const app = express();
-const PORT = 3000;
+require("dotenv").config();
+const PORT = process.env.PORT;
 
-app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+	res.send("Hello World");
+});
 
 app.get("/login/oauth2/code/google", (req, res) => {
 	res.json(decodeURIComponent(req.query.code));
 });
 
 app.listen(PORT, () => {
-	console.log("Server is running on port 8080");
+	console.log(`Server is running on port ${PORT}`);
 });
