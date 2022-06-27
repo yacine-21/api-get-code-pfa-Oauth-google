58f225c3ef12dcd6976ad1c4b58d89645f86243e
yacine-21 <yacinelyoubi37@gmail.com>
2022-06-27 12:27:49 +0200

    add / route

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const PORT = 8080
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 
+app.get("/",(req,re) => {
+	res.send("Hello world");
+}
+
 app.get("/login/oauth2/code/google", (req, res) => {
 	res.json(decodeURIComponent(req.query.code));
 });
